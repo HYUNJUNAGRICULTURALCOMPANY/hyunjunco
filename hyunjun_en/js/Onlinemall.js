01cd717a001c4e6747d084a80848a5dcf3cdcc95
HYUNJUNAGRICULTURALCOMPANY <87523731+HYUNJUNAGRICULTURALCOMPANY@users.noreply.github.com>
2021-09-09 18:51:51 +0900

    Onlinemall_M update
    Onlinemall_M update

--- hyunjun_en/js/Onlinemall.js
+++ hyunjun_en/js/Onlinemall.js
@@ -1,3 +1,36 @@
+if ($(window).width() < 640) {
+    $(document).ready(function () {
+        $('.m_menu').click(function () {
+            $(this).toggleClass('active');
+            $('.menu').toggleClass('on')
+        });
+        $(function () {
+            var lastScroll = 0;
+            $(window).scroll(function (event) {
+                var scroll = $(this).scrollTop();
+                if (scroll > 600) {
+                    $(".sec3").addClass("on");
+                }
+                else {
+                    $("sec3").removeClass("on");
+                }
+                lastScroll = scroll;
+            });
+            $(window).scroll(function (event) {
+                var scroll = $(this).scrollTop();
+                if (scroll > 650) {
+                    $(".sec4").addClass("on");
+                }
+                else {
+                    $(".sec4").removeClass("on");
+                }
+                lastScroll = scroll;
+            });
+        });
+        
+    });
+}
+
 var spot2 = $("header").offset().top;
 $(".g_top").click(function () {
     $("html, body").animate({
--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,12 @@
+if ($(window).width() < 640) {
+    $(document).ready(function () {
+        $('.m_menu').click(function () {
+            $(this).toggleClass('active');
+            $('.menu').toggleClass('on')
+        });
+    });
+}
+
 var spot2 = $("header").offset().top;
 $(".g_top").click(function () {
     $("html, body").animate({
